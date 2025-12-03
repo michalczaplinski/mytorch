b8ccb601869b504b3c63b6af14f07f1732172ef4
Michal Czaplinski <mmczaplinski@gmail.com>
2025-12-03 23:10:36 +0000

    Remove unnecessary sys.path manipulation in MNIST example scripts.

--- examples/mnist/mnist-adam.py
+++ examples/mnist/mnist-adam.py
@@ -3,10 +3,6 @@
 import numpy as np
 import sys
 
-import os
-# Ensure project root is on sys.path when running this script directly
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
 # Import utils
 from utils import fetch_mnist
 
